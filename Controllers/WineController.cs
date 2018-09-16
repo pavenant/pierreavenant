341f5dce3701365b4381cf428c148fa27ec79dcb
Pierre Avenant <pierre.avenant@gmail.com>
2018-09-16 16:02:24 +0100

    Add Wine Stuff

---
+++ Controllers/WineController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using pierreavenant.Models;
+
+namespace pierreavenant.Controllers
+{
+    public class WineController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<WineModel> wine = new List<WineModel>()
+            {
+                new WineModel
+                {
+                    Manufacturer = "Meerlust",
+                    Title = "Rubicon"
+                },
+                new WineModel
+                {
+                    Manufacturer = "Hermanus Pietersfontein",
+                    Title = "Die Arnoldus"
+                },
+                new WineModel
+                {
+                    Manufacturer = "Doran",
+                    Title = "Romney D"
+                },
+                new WineModel
+                {
+                    Manufacturer = "Springfield",
+                    Title = "A Work Of Time"
+                },
+                new WineModel
+                {
+                    Manufacturer = "Kaiken",
+                    Title = "Cab Sav"
+                }
+            };
+            return View(wine);
+        }
+    }
+}
